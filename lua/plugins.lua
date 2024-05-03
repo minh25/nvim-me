84b2dbbd6e445cb750f48a97ea799470df949599
minh25 <minh00012258@gmail.com>
2024-05-03 17:55:48 +0700

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,26 +2,8 @@ local plugins = {
 	"folke/which-key.nvim",
 	{ "folke/neoconf.nvim", cmd = "Neoconf" },
 	"folke/neodev.nvim",
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		opts = function()
-			require("options.nvimtree")
-		end,
-		config = function()
-			require("nvim-tree").setup({ opts })
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
+	require("options.nvim-tree"),
+	require("options.mason"),
 	require("options.conform"),
 }
 
