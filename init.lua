1517bdb7c8f08c1bed2fc6d7c1082a94ef565399
minh25 <minh00012258@gmail.com>
2024-05-03 17:36:58 +0700

    update

--- init.lua
+++ init.lua
@@ -1,22 +1,23 @@
+vim.g.mapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = require "plugins"
-local opts = require "options"
+local plugins = require("plugins")
+local opts = require("options")
 
 require("lazy").setup(plugins, opts)
 
 vim.schedule(function()
-  require "mappings"
+	require("mappings")
 end)
-
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,20 +1,28 @@
 local plugins = {
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
-  {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  opts = function()
-	  require "options.nvimtree"
-  end,
-  config = function()
-    require("nvim-tree").setup {opts}
-  end}
+	"folke/which-key.nvim",
+	{ "folke/neoconf.nvim", cmd = "Neoconf" },
+	"folke/neodev.nvim",
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = function()
+			require("options.nvimtree")
+		end,
+		config = function()
+			require("nvim-tree").setup({ opts })
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	require("options.conform"),
 }
 
 return plugins
